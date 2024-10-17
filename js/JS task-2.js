f169dd416ae457b6ed7fcccd69d44136788fc3ee
Alexander Andersen <157651781+AndersenAlexander@users.noreply.github.com>
2024-10-17 17:43:48 +0200

    Update JS task-2.js

--- js/JS task-2.js
+++ js/JS task-2.js
@@ -3,7 +3,8 @@ function getShippingMessage(country, price, deliveryFee) {
     return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
 
-// code for checking the correctness of the function
-console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
-console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"
+// task-2-results
+const task2Results = document.getElementById("task-2-results");
+task2Results.innerHTML += `<p>${getShippingMessage("Australia", 120, 50)}</p>`;
+task2Results.innerHTML += `<p>${getShippingMessage("Germany", 80, 20)}</p>`;
+task2Results.innerHTML += `<p>${getShippingMessage("Sweden", 100, 20)}</p>`;
